fcce23aba25dd35c4300da25bdc7f7280d85cb17
juliusv-metromart <kira@metromart.com>
2023-05-28 20:31:59 +0800

    added provider for retrofit, okhttp, and api service on network module

--- app/src/main/java/com/kira/composemvvmcoroutineshilt/di/NetworkModule.kt
+++ app/src/main/java/com/kira/composemvvmcoroutineshilt/di/NetworkModule.kt
@@ -1,9 +1,57 @@
 package com.kira.composemvvmcoroutineshilt.di
 
+import com.kira.composemvvmcoroutineshilt.network.ApiService
+import com.kira.composemvvmcoroutineshilt.network.Constants.BASE_URL
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Converter
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-class NetworkModule
+class NetworkModule {
+    @Singleton
+    @Provides
+    fun provideBaseUrl(): String {
+        return BASE_URL
+    }
+    @Singleton
+    @Provides
+    fun provideLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+    }
+    @Singleton
+    @Provides
+    fun provideConverterFactory(): Converter.Factory {
+        return GsonConverterFactory.create()
+    }
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient.Builder().addInterceptor(httpLoggingInterceptor).build()
+    }
+    @Singleton
+    @Provides
+    fun provideRetrofit(
+        baseUrl: String,
+        converterFactory: Converter.Factory,
+        okHttpClient: OkHttpClient
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .client(okHttpClient)
+            .addConverterFactory(converterFactory)
+            .build()
+    }
+    @Singleton
+    @Provides
+    fun provideApiService(retrofit: Retrofit): ApiService {
+        return retrofit.create(ApiService::class.java)
+    }
+}
